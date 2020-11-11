a8ed85543e906f7db92ee6d22aa27925184fadbf
otumian-empire <popecan1000@gmail.com>
2020-11-11 09:06:21 +0000

    Revert "ex21"
    This reverts commit 00d53acfc7b5184d86c7a867ff8494f949f6b5cb.

--- ex21/mathmodule3.py
+++
@@ -1,2 +0,0 @@
-"""
-For a third version, instead of the symbols, such as using, + , use add instead. Thus evaluate(+, 2, 5) , evaluate('+', '2', '5') and evaluate('add', 2, 5) or evaluate('add', '2', '5') works the same. """
